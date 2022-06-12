8df4f56c8a2e94f1a10bd7389a232b6ba18e9fda
Diego Fabian <fabian.diego@gmail.com>
2022-06-12 11:49:46 -0300

    mi primer commit

---
+++ main.py
@@ -0,0 +1,217 @@
+#print(1 // 2)
+#
+#def fun(x, y):
+#    if x== y:
+#        return x
+#    else:
+#        return fun(x, y-1)
+
+#print(fun(0, 3))
+
+#print(hola)
+
+#foo = (1,2,3)
+#foo.index(0)
+
+#from ast import In
+
+
+#my_list = [1,2]
+#for v in range(2):
+#    my_list.insert(-1, my_list[v])
+#
+#print(my_list)
+
+#z =0 
+#y=10
+#x=y < z and z > y or y > z and z < y
+
+
+from doctest import OutputChecker
+from re import X
+import re
+from tkinter import Y
+
+
+#foo = (1,2,3)
+#foo.index(0)    #valueerror
+
+#def fun(inp=2, out=3)
+#return inp * Out
+#print(fun(out=2))
+
+#def fun(x):
+  #  if x % 2 ==0:
+   #     return 1
+  #  else:
+ #       return 2
+#
+#print(fun(fun(2)))
+#2
+
+#x=3
+#y=2
+#x=x % y
+#x=x % y
+#y=y % X
+#print(y)
+#2 no
+
+#def fun(inp=2, out=3):
+#    return inp * out
+#print(fun(out=2))
+#4
+
+#dd = {"1":"0", "0": "1"}
+#for xin dd.vals():
+#    print(x, end"")
+
+#x = 2
+#y = 4
+#print(y ** (1/x))
+#2.0
+
+#print(1//2)
+#0
+
+
+#def func(a, b):
+ #   return b ** a
+#print(func(b=2, 2))
+#erroneo
+
+#m = [x * x for x in range(5)]
+#def fun(lst):
+#    del lst[lst[2]]
+#    return lst
+#print(fun(m))
+#[0,1,4,9]
+
+#lst = [[x for x in range(3)] for y in range(3)]
+#for r in range(3):
+#    for c in range(3):
+#        if lst [r][c] % 2 != 0:
+#            print("#")
+# 3
+
+#print("a", "b", "c", sep="sep")
+#asepbsepc
+
+#def fun(x, y):
+#   if x == y:
+#       return x
+#   else:
+#        return fun(x, y-1)
+#print(fun(0,3))
+#0
+
+#lst = [i for i in range(-1, -2)]
+#print(lst)
+# 0
+
+#[0,1,4,9]
+
+#nums = [1,2,3]
+#vals = nums
+#del vals[:]
+#print(nums, vals)
+#son iguales
+
+
+#def f_1(a):
+#    return None
+
+#def f_2(a):
+#    return f_1(a) * f_2(a)
+#
+#print(f_2(2))
+#error?
+
+#01
+
+#print(1 // 2)
+
+
+#dct = {}
+#dct["1"] = (1,2)
+#dct["2"] = (2,1)
+#for x in dct.keys():
+#    print(dct[x][1], end="")
+#21
+
+#def f(x):
+#    if x == 0:
+#        return 0
+#    return x + f(x-1)
+#print(f(3))
+# 6
+
+#def fun(x):
+#    if x % 2 ==0:
+#        return 1
+#    else:
+#        return
+#print(fun(fun(2)) + 1)
+#error
+
+#tup = (1,2,4,8)
+#tup = tup[1:-1]
+#tup= tup[0]
+#print(tup)
+#2
+
+def fun(x):
+    x += 1
+    return X
+x=2
+x= fun(x+1)
+print(x)
+#error no es
+
+#def fun(x,y,z):
+#    return x +2 * y +3 * z
+#print(fun(0, z=1, y=3))
+#9
+
+#def func(a, b):
+#    return a**a
+#print(func(2))
+#erroneo
+
+
+#def fun(x):
+#    global y
+#    y = x * x
+#    return y
+#fun(2)
+#print(y)
+#4
+
+#def any():
+#    print(var +1, end="")
+
+#var=1
+#any()
+#print(var)
+#21
+
+#entrada muy erronea
+
+#Tuplas, no es apprend
+
+#("one", "two, "three)   no es
+
+#mary had a ram .... no es
+
+#dic = {"uno" : "dos", "tres" : "uno", "dos" : "tres"}
+#v = dic["uno"]
+
+#for k in range(len(dic)):
+#    v = dic[v]
+#print(v)
+#dos
+
+#def fun(x, y, z):
+#    return x +2 * y + 3 * z
+#print(fun(0, z=1, y=3))
+#9
